6bcc79d2efccd123cf4167f4b9df1305dfc441e6
Gael nxv <ggenet@adneom.com>
2017-03-12 12:55:09 +0100

    irc connect

--- irc.js
+++ irc.js
@@ -3,12 +3,48 @@
 const irc = require('irc');
 
 const channels = [
-	'#moviegods',
-	'#mg-chat'
+	// '#moviegods',
+	// '#mg-chat'
 ];
 
+/*{
+    userName: 'nodebot',
+    realName: 'nodeJS IRC client',
+    port: 6667,
+    localAddress: null,
+    debug: false,
+    showErrors: false,
+    autoRejoin: false,
+    autoConnect: true,
+    channels: [],
+    secure: false,
+    selfSigned: false,
+    certExpired: false,
+    floodProtection: false,
+    floodProtectionDelay: 1000,
+    sasl: false,
+    retryCount: 0,
+    retryDelay: 2000,
+    stripColors: false,
+    channelPrefixes: "&#",
+    messageSplit: 512,
+    encoding: ''
+}*/
+
+const options = {
+	channels: channels,
+	autoConnect: false
+};
+
 const client = new irc.Client(
-	'irc.rizon.net', 
-	'Borrows', { 
-		channels 
-	});
+	'irc.rizon.no', 
+	'Borrows',
+	options
+);
+
+
+const retryCount = 99;
+
+client.connect(retryCount, function() {
+	console.log('here');
+});
